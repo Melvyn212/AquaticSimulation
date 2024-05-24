ed63c4fe05d174e358ce590ea07897a85b03c5f1
melvyn212 <dehundeabdala@gmail.com>
2024-05-24 08:38:15 +0200

    docker update

--- docker_run.sh
+++ docker_run.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-<<<<<<< HEAD
 #CODE_PATH="/home/adehundeag/wrkspace/AquaticSimulation"
 
 CODE_PATH="/home/hw8172/Bureau/Wrkspc/pomme/AquaticSimulation"
 
-=======
 CODE_PATH="/home/adehundeag/wrkspace/AquaticSimulation"
->>>>>>> 8109c956ed00f538c5255fbd3b55bd798637451c
+
 DOCKER_IMAGE="melvyn212/aquatic_simulation:latest"
 
 docker run -it --rm  \
